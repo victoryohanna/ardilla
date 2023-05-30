31c04dd3cff9a6e2f00a28ec5344a9c374866bd4
Victor Yohanna <victoryohanna@gmail.com>
2023-05-30 05:10:36 +0100

    modified transparency

--- src/components/transparency.jsx
+++ src/components/transparency.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import "../sass/transparency.scss";
 const Transparency = () => {
   return (
-    <div className="transparency-container">
+    <div className="transparency-container"> 
       <div className="container">
         <div className="row ">
           <div className="col-md-5">
@@ -68,31 +68,38 @@ const Transparency = () => {
                 </div>
                 <div className="interest-section">
                   <div className="mb-3">
-                    <label htmlFor="amount" className="form-label">
-                      Total
-                    </label>
-                    <span className="form-control form-control-sm">N6,600</span>
+                    <label htmlFor="amount" className="label">
+                      Total balance
+                    </label><br/>
+                    <span className="amount">&#8358;6,600</span>
                   </div>
                   <p>
-                    Saving N2000 monthly for 3 months will result ina balance of
-                    N6600{" "}
+                    Saving &#8358;2000 monthly for 3 months will result ina balance of
+                    &#8358;6600{" "}
                   </p>
                   <div className="mb-3">
-                    <label htmlFor="amount" className="form-label">
-                      Total
-                    </label>
-                    <span className="form-control form-control-sm">
-                      N600 [2%]
+                    <label htmlFor="amount" className="label">
+                      Interest
+                    </label> <br/>
+                    <span className="amount">
+                    &#8358;600 [2%]
                     </span>
                   </div>
                   <div className="mb-3">
-                    <label htmlFor="amount" className="form-label">
-                      Total
-                    </label>
-                    <span className="form-control form-control-sm">N600</span>
+                    <label htmlFor="amount" className="label">
+                      Total Savings
+                    </label><br/>
+                    <span className="amount">&#8358;600</span>
                   </div>
+                  <div className="chart-section">
+                
+              </div>
+              <div className="btn-section">
+                <button type="submit" className="btn "> Start Saving Now</button>
+              </div>
                 </div>
               </form>
+              
             </div>
           </div>
         </div>
